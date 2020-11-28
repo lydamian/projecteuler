0ed3b804768ff4cde1afac5b58e91d42811ac5a8
Damian Ly <lydamian@gmail.com>
2020-11-27 17:04:12 -0800

    implemented problem1 of projecteuler.net

--- problem2/main.go
+++ problem2/main.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
-func fib(x int) int {
-	if fib
+func getFibValues(max int) []int {
+	fibValues := []int{1,2}
+	fibValue := fibValues[len(fibValues) - 1] + fibValues[len(fibValues) - 2]
+	for fibValue < max {
+		fibValues = append(fibValues, fibValue)
+		fibValue = fibValues[len(fibValues) - 1] + fibValues[len(fibValues) - 2]
+	}
+	return fibValues
+}
+
+func sumEvenValues(mySlice []int) int {
+	sum := 0
+	for _, v := range mySlice {
+		if v % 2 == 0 {
+			sum += v
+		}
+	}
+	return sum
 }
 
 func main() {
-    fmt.Println("hello world")
+	fibValues := getFibValues(4000000)
+	fmt.Println(fibValues)
+	fibSum := sumEvenValues(fibValues)
+	fmt.Println(fibSum)
 }
